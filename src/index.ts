4427dfdcb24dc2911fad8fd455bf2cd144df56db
eladr7 <eladr7@gmail.com>
2023-07-20 15:39:02 +0300

    Impl Placeholder logic for num_pools

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,28 @@
 import { Block, StargateClient } from "@cosmjs/stargate";
-
+import { osmosis } from "osmojs";
+const { createRPCQueryClient } = osmosis.ClientFactory;
 const sleep = async (ms: number) =>
   new Promise((resolve) => setTimeout(resolve, ms));
 
-const getNumPools = async (): Promise<number> => {
-  // Placeholder. Will be changed later to the true logic
-  return Math.floor(Math.random() * 1000);
+const getNumPools = async (osmosisClient: any): Promise<number> => {
+  // Query the osmosis pools
+  const response = await osmosisClient.osmosis.gamm.v1beta1.pools();
+  return response.pools.length;
 };
 
 export const blockQuerier = async () => {
-  const client = await StargateClient.connect(
+  const stargateClient = await StargateClient.connect(
     "https://rpc.osmotest5.osmosis.zone"
   );
-  let lastQueriedHeight = 0;
+  const osmosisClient = await createRPCQueryClient({
+    rpcEndpoint: "https://rpc.osmotest5.osmosis.zone",
+  });
 
   // Continuously check for the latest block
+  let lastQueriedHeight = 0;
   while (true) {
     // Getting the latest block
-    const block: Block = await client.getBlock();
+    const block: Block = await stargateClient.getBlock();
 
     // If block height was updated is divisible by 10, query number of pools
     if (
@@ -25,7 +30,7 @@ export const blockQuerier = async () => {
       block.header.height % 10 === 0
     ) {
       lastQueriedHeight = block.header.height;
-      const numPools = await getNumPools();
+      const numPools = await getNumPools(osmosisClient);
 
       console.log("Block height: ", block.header.height);
       console.log("Block hash: ", block.id);
